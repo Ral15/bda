f5493c0ad1083461d8cdb58764052ff306787c08
Raul Mar <raulmar_94@hotmail.com>
2017-02-14 21:45:06 -0600

    ventas ordenes qro

---
+++ ventas_qro.sql
@@ -0,0 +1,246 @@
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (1, 3, 2, 2, 180);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 1);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 2, 1);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (2, 1, 2, 16, 110);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 2);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 2);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 2);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (3, 3, 2, 12, 90);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 3);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (4, 3, 2, 16, 320);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 4);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 4);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 4);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 4);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (5, 3, 2, 4, 80);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 5);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (6, 2, 2, 9, 160);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 6);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 6);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (7, 2, 2, 17, 80);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 7);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (8, 2, 2, 20, 95);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 8);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 8);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (9, 1, 2, 6, 90);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 9);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (10, 2, 2, 17, 80);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 10);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (11, 2, 2, 10, 160);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 11);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 11);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (12, 3, 2, 1, 270);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 12);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 12);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 12);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (13, 2, 2, 10, 160);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 13);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 13);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (14, 1, 2, 6, 92);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 14);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 9, 14);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (15, 1, 2, 20, 110);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 15);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 15);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 15);
+
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (16, 3, 2, 15, 180);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 16);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 2, 16);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (17, 2, 2, 9, 95);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 17);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 17);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (18, 2, 2, 15, 270);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 18);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 18);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 18);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (19, 2, 2, 14, 180);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 19);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 2, 19);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (20, 2, 2, 18, 180);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 20);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 2, 20);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (21, 1, 2, 16, 260);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 21);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 21);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 21);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (22, 1, 2, 16, 260);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 22);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 22);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 22);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (23, 2, 2, 15, 90);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 23);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (24, 3, 2, 7, 80);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 24);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (25, 2, 2, 20, 100);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 4, 25);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (26, 2, 2, 20, 110);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 26);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 26);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 26);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (27, 3, 2, 13, 270);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 27);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 27);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 27);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (28, 1, 2, 19, 90);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 28);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (29, 3, 2, 16, 180);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 29);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 2, 29);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (30, 3, 2, 1, 240);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 30);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 30);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 30);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (31, 2, 2, 11, 160);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 31);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 31);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (32, 3, 2, 15, 270);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 32);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 32);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 32);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (33, 3, 2, 12, 95);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 33);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 33);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (34, 3, 2, 1, 110);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 34);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 34);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 34);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (35, 3, 2, 10, 240);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 35);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 35);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 35);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (36, 2, 2, 16, 90);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 36);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (37, 3, 2, 20, 95);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 37);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 37);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (38, 2, 2, 13, 240);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 38);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 38);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 38);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (39, 1, 2, 11, 95);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 39);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 39);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (40, 1, 2, 18, 180);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 40);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 2, 40);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (41, 3, 2, 2, 260);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 41);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 41);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 41);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (42, 3, 2, 9, 270);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 42);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 42);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 42);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (43, 1, 2, 13, 110);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 43);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 43);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 43);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (44, 2, 2, 11, 92);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 44);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 9, 44);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (45, 2, 2, 16, 180);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 45);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 2, 45);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (46, 1, 2, 10, 180);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 46);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 2, 46);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (47, 3, 2, 7, 160);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 47);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 47);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (48, 1, 2, 4, 270);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 48);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 3, 48);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 48);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (49, 2, 2, 9, 180);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 49);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 2, 49);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (50, 1, 2, 20, 80);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 50);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (51, 2, 2, 16, 92);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 51);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 9, 51);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (52, 3, 2, 6, 320);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 52);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 52);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 52);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 52);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (53, 3, 2, 2, 90);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 5, 53);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (54, 3, 2, 3, 95);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 54);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 8, 54);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (55, 2, 2, 18, 80);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 55);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (56, 1, 2, 3, 100);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 4, 56);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (57, 3, 2, 14, 80);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 57);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (58, 3, 2, 18, 160);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 58);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 58);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (59, 3, 2, 13, 100);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 4, 59);
+
+insert into e7venta (id, empleado_id, sucursal_id, cliente_id, monto) values (60, 2, 2, 11, 160);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 60);
+insert into e7orden (id, producto_id, venta_id) values (seq_orden.nextval, 1, 60);
+
