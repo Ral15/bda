640e31623f7e3dc8d8158bf9e65b999e86ae53ef
Raul Mar <raulmar_94@hotmail.com>
2017-02-19 11:13:00 -0600

    procedure transerferir_ingredientes created

---
+++ procedures/transferir_ingrediente.sql
@@ -0,0 +1,76 @@
+CREATE OR REPLACE PROCEDURE TRANSFERIRINGREDIENTE
+  (
+    id_sucursal_destino IN NUMBER,
+    item_idd             IN NUMBER,
+    cantidad_quiero     IN NUMBER
+  ) IS
+  cantidad_del_otro   NUMBER;
+  BEGIN
+    -- CDMX
+    IF id_sucursal_destino = 1 THEN
+      --sacar cantidad del item
+      select cantidad into cantidad_del_otro 
+      from e7inventario@cdmx
+      where e7inventario.item_id@cdmx = item_idd;
+
+      -- si no tiene suficiente cantidad para darme
+      IF cantidad_del_otro <= cantidad_quiero THEN
+        RAISE_APPLICATION_ERROR(-20000, 'no tengo suficiente para darte!');
+      ELSE
+        -- restar cantidad de inventario destino
+        update e7inventario@cdmx 
+        set cantidad@cdmx = cantidad@cdmx - cantidad_quiero
+        where e7inventario.item_id@cdmx = item_idd;
+
+        -- sumar cantidad a inventario origen
+        update e7inventario
+        set cantidad = cantidad + cantidad_quiero
+        where item_id = item_idd;
+      END IF;
+    -- -- QRO
+    ELSIF id_sucursal_destino = 2 THEN
+      --sacar cantidad del item
+      select cantidad into cantidad_del_otro 
+      from e7inventario@p_qro
+      where e7inventario.item_id@p_qro = item_idd;
+
+      -- si no tiene suficiente cantidad para darme
+      IF cantidad_del_otro <= cantidad_quiero THEN
+        RAISE_APPLICATION_ERROR(-20000, 'no tengo suficiente para darte!');
+      ELSE
+        -- restar cantidad de inventario destino
+        update e7inventario@p_qro 
+        set cantidad@p_qro = cantidad@p_qro - cantidad_quiero
+        where e7inventario.item_id@p_qro = item_idd;
+
+        -- sumar cantidad a inventario origen
+        update e7inventario
+        set cantidad = cantidad + cantidad_quiero
+        where item_id = item_idd;
+      END IF;
+    -- -- slp
+    ELSIF id_sucursal_destino = 3 THEN
+      --sacar cantidad del item
+      select cantidad into cantidad_del_otro 
+      from e7inventario@slp
+      where e7inventario.item_id@slp = item_idd;
+
+      -- si no tiene suficiente cantidad para darme
+      IF cantidad_del_otro <= cantidad_quiero THEN
+        RAISE_APPLICATION_ERROR(-20000, 'no tengo suficiente para darte!');
+      ELSE
+        -- restar cantidad de inventario destino
+        update e7inventario@slp 
+        set cantidad@slp = cantidad@slp - cantidad_quiero
+        where e7inventario.item_id@slp = item_idd;
+
+        -- sumar cantidad a inventario origen
+        update e7inventario
+        set cantidad = cantidad + cantidad_quiero
+        where item_id = item_idd;
+      END IF;
+    ELSE
+      RAISE_APPLICATION_ERROR(-20000, 'parametros invalidos, verificar');
+    END IF;
+  COMMIT;
+END TRANSFERIRINGREDIENTE;
